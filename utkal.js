7a6d7cfed24bfac607de54320c7a03bb04f74779
Saktiranjan693 <saktiranjanbarik45@gmail.com>
2026-03-22 22:51:57 +0530

    First University of Odisha 17th oldest University in India

---
+++ utkal.js
@@ -0,0 +1,6 @@
+const bgimg=document.getElementById("logo");
+
+
+bgimg.style.backgroundImage="url(./images/imagesutkal/uu-75-banner.jpg)";
+bgimg.style.backgroundSize="cover";
+bgimg.style.backgroundPosition = "center";
